e077af5585dc8e93dcdbf45f27b59a82ca0bc04f
Namy01 <namy91646@gmail.com>
2025-05-15 06:46:00 +0545

    edit gallery added

--- gallery_app/urls.py
+++ gallery_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import Gallery_list
+from .views import Gallery_list, Gallery_edit
 
 urlpatterns = [
     path('', Gallery_list.as_view(), name='gallery_list'),
+    path('edit-gallery/<int:id>/', Gallery_edit.as_view(), name='edit-gallery')
+
 ]
--- gallery_app/views.py
+++ gallery_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView , UpdateView
 from .models import Gallery
 
 class Gallery_list(ListView):
@@ -7,4 +8,11 @@ class Gallery_list(ListView):
     template_name = "gallery/gallery_list.html"
     context_object_name = "galleries"
 
+class Gallery_edit(UpdateView):
+    model = Gallery
+    template_name = "gallery/gallery_edit.html"
+    fields = ["title", "cover_image"]
+    success_url = '/'
 
+    def get_object(self, queryset=None):
+        return Gallery.objects.get(id=self.kwargs['id'])
